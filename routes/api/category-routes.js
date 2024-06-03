eed51aee60dc719ef7b676a9d01a0a7334d2d48b
Kyler Gibbs <kylergibbs@hotmail.com>
2024-06-03 14:26:36 -0700

    Finished category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,6 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
     try {
     const cats = await Category.findAll({
       include: [{model: Product}],
@@ -16,21 +14,54 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+router.get('/:id', async (req, res) => {
 
+  try {
+    const cat = await Category.findOne({
+      where: {id: req.params.id},
+      include: [{model: Product}],
+    });
+    res.status(200).json(cat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  }) .then((newCat) => {
+    res.json(newCat);
+  }) .catch ((err) => {
+    res.json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name,
+  },
+  {
+    where: {id: req.params.id}
+  },
+) .then((upCat) => {
+    res.json(upCat);
+  }) .catch ((err) => {
+    res.json(err);
+  });
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id:req.params.id,
+    },
+  })  .then((delCat) => {
+    res.json(delCat);
+  }) .catch ((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
